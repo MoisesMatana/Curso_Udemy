c0638f1ea66989ba0913123bac321a4d4ca450b6
MoisesMatana <musmatan@ucs.br>
2019-07-21 01:02:58 -0300

    Seção 8_enumeracoes

---
+++ src/aplicacao/Exe1_Enumeracoes.java
@@ -0,0 +1,11 @@
+package aplicacao;
+
+import java.util.Locale;
+
+public class Exe1_Enumeracoes {
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		// Scanner sc = new Scanner(System.in);
+
+	}
+}
---
+++ src/entidades/Trabalhador.java
@@ -0,0 +1,87 @@
+package entidades;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import entidades.enums.NivelTrabalhador;
+
+public class Trabalhador {
+	private String nome;
+	private NivelTrabalhador level;
+	private Double salarioBase;
+
+	private Departamento depto;
+	private List<ContratoHora> contratos = new ArrayList<>();
+
+	public Trabalhador() {
+	}
+
+	public Trabalhador(String nome, NivelTrabalhador level, Double salarioBase, Departamento depto) {
+		this.nome = nome;
+		this.level = level;
+		this.salarioBase = salarioBase;
+		this.depto = depto;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public NivelTrabalhador getLevel() {
+		return level;
+	}
+
+	public void setLevel(NivelTrabalhador level) {
+		this.level = level;
+	}
+
+	public Double getSalarioBase() {
+		return salarioBase;
+	}
+
+	public void setSalarioBase(Double salarioBase) {
+		this.salarioBase = salarioBase;
+	}
+
+	public Departamento getDepto() {
+		return depto;
+	}
+
+	public void setDepto(Departamento depto) {
+		this.depto = depto;
+	}
+
+	public List<ContratoHora> getContratos() {
+		return contratos;
+	}
+
+	public void addContrato(ContratoHora contrato) {
+		contratos.add(contrato);
+	}
+
+	public void removeContrato(ContratoHora contrato) {
+		contratos.remove(contrato);
+	}
+
+	public double calcular(int ano, int mes) {
+		double soma = salarioBase;
+		Calendar cal = Calendar.getInstance();
+
+		for (ContratoHora contratoHora : contratos) {
+			cal.setTime(contratoHora.getData());
+			int contrato_ano = cal.get(Calendar.YEAR);
+			int contrato_mes = cal.get(Calendar.MONTH);
+
+			if (contrato_ano == ano && contrato_mes == mes) {
+				soma += contratoHora.valorTotal();
+			}
+		}
+		return soma;
+	}
+
+}
