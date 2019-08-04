55175c635e89e0ad14be3656c49330c058f11dad
MoisesMatana <musmatan@ucs.br>
2019-08-04 20:26:09 -0300

    Teste

--- src/entidades/Postagem.java
+++ src/entidades/Postagem.java
@@ -8,4 +8,42 @@ public class Postagem {
 	private String conteudo;
 	private Integer likes;
 	
+	public Postagem(Date momento, String titulo, String conteudo, Integer likes) {
+		this.momento = momento;
+		this.titulo = titulo;
+		this.conteudo = conteudo;
+		this.likes = likes;
+	}
+
+	public Date getMomento() {
+		return momento;
+	}
+
+	public void setMomento(Date momento) {
+		this.momento = momento;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getConteudo() {
+		return conteudo;
+	}
+
+	public void setConteudo(String conteudo) {
+		this.conteudo = conteudo;
+	}
+
+	public Integer getLikes() {
+		return likes;
+	}
+
+	public void setLikes(Integer likes) {
+		this.likes = likes;
+	}
 }
